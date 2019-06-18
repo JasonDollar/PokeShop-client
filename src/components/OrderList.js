a0e93dd2d9664cbba6feaae1c9127ea3e47976f3
JasonDollar <elrey33@gmail.com>
2019-06-18 12:05:50 +0200

    styling order items, update the styled-components

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import styled from 'styled-components'
+import { format } from 'date-fns'
 import WidthContainer from './styles/WidthContainer'
 
 export const USER_ORDERS_QUERY = gql`
@@ -8,6 +10,7 @@ export const USER_ORDERS_QUERY = gql`
     orders {
       id
       price
+      createdAt
       user {
         name
       }
@@ -18,12 +21,65 @@ export const USER_ORDERS_QUERY = gql`
         pokemon {
           id
           name
+          image
         }
         # seller {
         #   id
         #   name
         # }
       }
+    }
+  }
+`
+
+const OrdersUl = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 2rem;
+  margin: 2rem 0;
+  padding: 0;
+`
+
+const OrderItem = styled.li`
+  list-style: none;
+  /* border: 1px solid ${props => props.theme.colorLightGrey}; */
+  box-shadow: 0 2px 5px 0 ${props => props.theme.colorGrey};
+  padding: 1rem;
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 2px solid ${props => props.theme.primaryRed};
+    margin-bottom: 1rem;
+    padding: .5rem;
+    padding-bottom: 1.5rem;
+    & .title {
+      margin: 0;
+    }
+    & .date {
+      margin: 0;
+      color: ${props => props.theme.colorLightGrey}
+    }
+  }
+  .total {
+    font-size: 2.5rem;
+    margin: 1rem;
+  }
+`
+
+const SinglePokemonInfo = styled.li`
+  display: flex;
+  align-items: center;
+  list-style: none;
+    font-size: 2rem;
+  .pokemonImage {
+    width: 5rem;
+    height: 5rem;
+  }
+  & p span.pokemonName {
+    display: inline-block;
+    &::first-letter {
+      text-transform: uppercase;
     }
   }
 `
@@ -36,21 +92,30 @@ const OrderList = () => (
         if (error) return (<p>{error.message}</p>)
         
         if (data.orders.length <= 0) return <p>No orders found</p>
+        console.log(data)
         return (
-            <div>
+            <OrdersUl>
               {data.orders.map(order => (
-                <Fragment key={order.id}>
-                  <h3>Order {order.id}</h3>
-                  <p>Price: {order.price}</p>
+                <OrderItem key={order.id}>
+                  <header>
+                    <h3 className="title">Order {order.id}</h3>
+                    <p className="date">{format(parseInt(order.createdAt), 'DD/MM/YYYY H:mm')}</p>
+                  </header>
+                  
                   <ul>
                     {order.items.map(item => (
-                      <li key={item.id}>{item.pokemon.name}</li>
+                      <SinglePokemonInfo key={item.id}>
+                        <img src={item.pokemon.image} alt={item.pokemon.name + ' image'} className="pokemonImage" />
+                        <p>
+                          <span className="pokemonName">{item.pokemon.name}</span> x{item.quantity} - <strong>{item.price}CR</strong>
+                        </p>
+                      </SinglePokemonInfo>
                     ))}
                   </ul>
-
-                </Fragment>
+                  <p className="total">Price: {order.price}</p>
+                </OrderItem>
               ))}
-            </div>
+            </OrdersUl>
         )
       }}
     </Query>
